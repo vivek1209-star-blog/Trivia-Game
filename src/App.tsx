10723518599e4f4705e41e75b32e0e680c390bf5
vivek mishra <52.2018.vivekmishra@gmail.com>
2022-07-21 22:41:42 +0530

    added react router dom and components and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { BrowserRouter } from "react-router-dom";
-import './App.css';
-import { Footer } from './components/Footer/Footer';
-import { Header } from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from "./pages/Home/Home";
+import { Quiz } from "./pages/Quiz/Quiz";
+import { Result } from "./pages/Result/Result";
 
 function App() {
+
   return (
     <BrowserRouter>
-    <div className="App" style={{ backgroundImage: 'url("/ques1.png")' }}>
-      <Header />
-    </div>
-    <Footer />
+      <div className="App" style={{ backgroundImage: 'url("/ques1.png")' }}>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home name={"vivek"} setName={"Mishra"} fetchQuestions={"abc"} />
+            }
+          />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
   );
 }
