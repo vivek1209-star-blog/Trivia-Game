520e13d2347775b3bb4c679c3e8dc0f8d6d1000c
vivek mishra <52.2018.vivekmishra@gmail.com>
2022-07-21 18:30:28 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
     <div className="App" style={{ backgroundImage: 'url("/ques1.png")' }}>
       <Header />
     </div>
+    <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made by Vivek Mishra
+    </div>
+  );
+};
